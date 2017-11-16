0eb7f81d78916755bc2a418a64123f58e65d39f5
jamieberrier <berrier.jamie@gmail.com>
2017-11-16 19:49:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,5 +4,7 @@ const driver = {};
 function updateDriverWithKeyAndValue(driver, key, value){
   driver[key] = value;
 
-  return driver;
+  const newDriver = { ...driver };
+
+  return newDriver;
 }
